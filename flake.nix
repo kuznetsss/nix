509308a304cbbbe8ad92f23d87eb8bd1e5c4acea
Sergey <kuzzz99@gmail.com>
2023-09-13 08:33:08 +0100

    Separate darwin and home

--- flake.nix
+++ flake.nix
@@ -11,27 +11,42 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
     let
-      callPackage = nixpkgs.legacyPackages.aarch64-darwin.callPackage;
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      callPackage = pkgs.callPackage;
     in
     {
       darwinConfigurations."Sergeys-Laptop" = nix-darwin.lib.darwinSystem {
         modules = [
           ./darwin.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            users.users.sergey = {
-              name = "sergey";
-              home = "/Users/sergey";
-            };
-            home-manager.users.sergey = import ./home.nix;
-          }
+          # home-manager.darwinModules.home-manager
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #   users.users.sergey = {
+          #     name = "sergey";
+          #     home = "/Users/sergey";
+          #   };
+          #   home-manager.users.sergey = import ./home.nix;
+          # }
         ];
       };
 
       # Expose the package set, including overlays, for convenience.
-      darwinPackages = self.darwinConfigurations."Sergeys-Laptop".pkgs;
+      # darwinPackages = self.darwinConfigurations."Sergeys-Laptop".pkgs;
+
+      homeConfigurations."sergey" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # home-manager.useGlobalPkgs = true;
+        # home-manager.useUserPackages = true;
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+            ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
 
       formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
     };
