aef507cb219d3a29f7df303aece5c3b9fa72de37
Sergey Kuznetsov <skuznetsov@ripple.com>
2023-11-16 10:33:15 +0000

    Add packages to w

--- home/w/packages.nix
+++ home/w/packages.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    llvmPackages_14.libcxxClang
     clang-tools_16
     cmake-language-server
     cmake
@@ -7,7 +8,15 @@
     lldb
     ccache
 
+    python311Full
     python311Packages.python-lsp-server
+    python311Packages.black
+    python311Packages.flake8
+    python311Packages.mccabe
+    python311Packages.pylsp-rope
+    python311Packages.python-lsp-black
+    python311Packages.matplotlib
+
     gh
     jq
 
