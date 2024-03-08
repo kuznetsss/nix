602e192de31cd750d6e3c8225aa1936c30012f72
Sergey <kuzzz99@gmail.com>
2024-03-08 19:03:27 +0000

    Add network configuration for ivan

---
+++ hosts/ivan/network.nix
@@ -0,0 +1,9 @@
+age-encryption.org/v1
+-> X25519 9MxSyOnuCXGk/aHWysqctUtYu+30Yf2YrFFXADUjwxM
+va6l60WQPIMo3BdKKtFQ5uf/sO04REvg+cpxKtW+k5Y
+-> kyN`\C-grease -{
+v1YAxC66qIuBBb3r3IYAdhc1qCBBYM/Weu7aWCO2TShMzktzADArwxfa8uoeI777
+e12ly8sZXqqbPbZ3pN7SyqgxhFp4kW5lYuEk2YcgIajrO4XNSL/5l+2T5ZYbazDq
+
+--- LlTc6mHzjQxKiCKSRb0a8EeMFHk+S2KfVhEA3OytQN8
+Í©ÈŸ gçÎõ È~^jÂ„šuÄqŒfİ^z^>»zn*´I01n£ñG¸Ki‘£T¼5Àz—úÂjN½W•£æHéúJ&:0ˆçÎcŸ
