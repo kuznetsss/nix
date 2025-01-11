238c2df603bc33d027d380fa9e880e7f6e6d3930
Sergey <kuzzz99@gmail.com>
2025-01-11 00:21:44 +0000

    Improve wezterm config

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -3,35 +3,154 @@
     enable = true; # use from brew instead
     enableZshIntegration = false;
     extraConfig = ''
-      local wezterm = require 'wezterm'
+      local colors = {
+        tab_bar = {
+          background = '#272727',
+          active_tab = {
+            bg_color = '#353535',
+            fg_color = '#bbbbbb',
+            intensity = 'Bold',
+          },
+
+          -- Inactive tabs are the tabs that do not have focus
+          inactive_tab = {
+            bg_color = '#272727',
+            fg_color = '#aaaaaa',
+          },
+        },
+      }
+
+      local keys = {
+        {
+          mods = 'CMD',
+          key = 's',
+          action = wezterm.action.ActivateCopyMode,
+        },
+        {
+          mods = 'CMD',
+          key = 'n',
+          action = wezterm.action.ActivateTabRelative(1),
+        },
+        {
+          mods = 'CMD',
+          key = 'p',
+          action = wezterm.action.ActivateTabRelative(-1),
+        },
+        {
+          mods = 'CMD',
+          key = 'x',
+          action = wezterm.action.CloseCurrentPane { confirm = true },
+        },
+        {
+          mods = 'CMD',
+          key = 'w',
+          action = wezterm.action.CloseCurrentTab { confirm = true },
+        },
+        {
+          mods = 'CMD',
+          key = 'a',
+          action = wezterm.action.QuickSelect,
+        },
+        {
+          mods = 'CMD',
+          key = 'u',
+          action = wezterm.action.ScrollByPage(-0.8),
+        },
+        {
+          mods = 'CMD',
+          key = 'd',
+          action = wezterm.action.ScrollByPage(0.8),
+        },
+        {
+          mods = 'CMD',
+          key = '/',
+          action = wezterm.action.Search { CaseInSensitiveString = "" },
+        },
+        {
+          mods = 'CMD',
+          key = 'y',
+          action = wezterm.action.ShowTabNavigator,
+        },
+        {
+          mods = 'CMD',
+          key = 't',
+          action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+        },
+        {
+          mods = 'CMD',
+          key = 'i',
+          action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+        },
+        {
+          mods = 'CMD',
+          key = 'o',
+          action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+        },
+        {
+          mods = 'CMD',
+          key = 'g',
+          action = wezterm.action.ScrollToBottom,
+        },
+        {
+          mods = 'CMD',
+          key = 'G',
+          action = wezterm.action.ScrollToTop,
+        },
+        {
+          mods = 'LEADER',
+          key = 'n',
+          action = wezterm.action.MoveTabRelative(1),
+        },
+        {
+          mods = 'LEADER',
+          key = 'p',
+          action = wezterm.action.MoveTabRelative(-1),
+        },
+        {
+          mods = 'LEADER',
+          key = 'Space',
+          action = wezterm.action.RotatePanes 'CounterClockwise',
+        },
+        {
+          mods = 'CMD',
+          key = 'z',
+          action = wezterm.action.TogglePaneZoomState,
+        },
+        {
+          mods = 'LEADER',
+          key = 'r',
+          action = wezterm.action.PromptInputLine {
+            description = 'Enter new name for tab',
+            action = wezterm.action_callback(function(window, pane, line)
+              if line then
+                window:active_tab():set_title(line)
+              end
+            end),
+          },
+        }
+      }
+
+      for key, dir in pairs { h = 'Left', j = 'Down', k = 'Up', l = 'Right' } do
+         table.insert(keys, {
+           mods = 'CMD',
+           key = key,
+           action = wezterm.action.ActivatePaneDirection(dir),
+         })
+         table.insert(keys, {
+           mods = 'LEADER',
+           key = key,
+           action = wezterm.action.AdjustPaneSize { dir, 5 },
+         })
+      end
+
       return {
         allow_square_glyphs_to_overflow_width = "Always",
         color_scheme = 'Tomorrow (dark) (terminal.sexy)',
-        colors = {
-          tab_bar = {
-            background = '#272727',
-            active_tab = {
-              bg_color = '#353535',
-              fg_color = '#bbbbbb',
-
-              -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-              -- label shown for this tab.
-              -- The default is "Normal"
-              intensity = 'Bold',
-              underline = 'None',
-              italic = false,
-              strikethrough = false,
-            },
-
-            -- Inactive tabs are the tabs that do not have focus
-            inactive_tab = {
-              bg_color = '#272727',
-              fg_color = '#aaaaaa',
-            },
-          },
-        },
+        colors = colors,
         default_gui_startup_args = { 'connect', 'unix' },
         hide_tab_bar_if_only_one_tab = false,
+        keys = keys,
+        leader = { mods = 'CTRL', key = 'b' },
         font = wezterm.font('JetBrains Mono', {weight='Regular'}),
         font_rules = {
           {
@@ -61,13 +180,9 @@
           },
         },
         font_size = 14.0,
-        foreground_text_hsb = {
-          hue = 1.0,
-          saturation = 1.0,
-          brightness = 1.0,
-        },
         force_reverse_video_cursor = true,
         native_macos_fullscreen_mode = true,
+        tab_bar_at_bottom = true,
         window_padding = {
           left = 2,
           right = 0,
