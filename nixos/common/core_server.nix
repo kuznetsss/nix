976b48546f68298adacee0a3258eec76a319c12d
Sergey Kuznetsov <kuzzz99@gmail.com>
2025-12-05 13:00:46 +0000

    Create core_server common config

---
+++ nixos/common/core_server.nix
@@ -0,0 +1,87 @@
+{ pkgs, hostName, private, ... }:
+let
+  networkConfig = private.network.${hostName};
+  sshPort = private.ssh.port;
+in
+  {
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_US.UTF-8";
+  services.journald.extraConfig = "SystemMaxUse=2G";
+
+  networking = {
+    inherit hostName;
+    useDHCP = false;
+    firewall = {
+      enable = true;
+      logRefusedConnections = false;
+      allowPing = true;
+      extraCommands = "iptables -A INPUT -i tailscale0 -p tcp -m tcp --dport ${
+          toString sshPort
+        } -j ACCEPT";
+    };
+  };
+
+  systemd.network = {
+    enable = true;
+    networks."10-ens3" = {
+      matchConfig.Name = "ens3";
+      address =
+        [ (networkConfig.ip + "/" + toString networkConfig.prefixLength) ];
+      routes = [{ Gateway = networkConfig.gateway; }];
+    };
+  };
+
+  users.users = {
+    sergey = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      initialPassword = "some_passwd";
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [ private.ssh.home_mac_pub_key ];
+    };
+    deployer = {
+      isNormalUser = true;
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [ private.ssh.home_mac_pub_key ];
+    };
+  };
+
+  security.sudo.extraConfig = ''
+    deployer ALL = (root) NOPASSWD:/run/current-system/sw/bin/nixos-rebuild
+  '';
+
+  nix = {
+    optimise.automatic = true;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 3d";
+    };
+  };
+  environment.systemPackages = with pkgs; [ neovim htop git ];
+
+  programs.zsh.enable = true;
+
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 3 * 512; # 1.5GB
+  }];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+    ports = [ sshPort ];
+    openFirewall = false;
+  };
+
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "server";
+  };
+
+  system.stateVersion = "25.11";
+}
