34dcce52dc75d2793ed48b5b051f0216ec8f5082
Sergey Kuznetsov <kuzzz99@gmail.com>
2026-03-04 22:12:47 +0000

    Comment claude packages

--- home/common/packages.nix
+++ home/common/packages.nix
@@ -1,26 +1,4 @@
 { pkgs, ... }:
-let
-  claude-code-acp-latest = pkgs.buildNpmPackage {
-    pname = "claude-code-acp";
-    version = "0.19.2";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "zed-industries";
-      repo = "claude-agent-acp";
-      tag = "v0.19.2";
-      hash = "sha256-MGy6hdOwIASh4qirCaQBF2czVWEkYiRzqXMm9qun5Tk=";
-    };
-
-    npmDepsHash = "sha256-UZJZfGmmbHKSlRXmIC5hqZNTJ5k3EQO79mGjWaKtgDE=";
-
-    meta = {
-      description = "ACP-compatible coding agent powered by the Claude Code SDK";
-      homepage = "https://github.com/zed-industries/claude-agent-acp";
-      license = pkgs.lib.licenses.asl20;
-      mainProgram = "claude-code-acp";
-    };
-  };
-in
 {
   home.packages =
     with pkgs;
@@ -42,8 +20,9 @@ in
       jjui
       gh
       gnupg
-      claude-code
-      claude-code-acp-latest
+      # Install by brew for now
+      #claude-code
+      #claude-code-acp
 
       nix
       nixd
