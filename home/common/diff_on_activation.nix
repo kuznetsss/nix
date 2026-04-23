91acd1e338de9f200379ea4166956270c691dc3d
Sergey Kuznetsov <kuzzz99@gmail.com>
2026-04-23 23:50:36 +0100

    Small improvement

--- home/common/diff_on_activation.nix
+++ home/common/diff_on_activation.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }:
 {
-  home.activation.report-changes = lib.hm.dag.entryAnywhere ''
+  home.activation.report-changes = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [[ -n "''${oldGenPath:-}" && "$oldGenPath" != "$newGenPath" ]]; then
       ${pkgs.nvd}/bin/nvd diff "$oldGenPath" "$newGenPath"
     fi
